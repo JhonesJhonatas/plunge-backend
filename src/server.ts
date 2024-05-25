14b9aeb2a8c28ed771849fa010fe745f88a9d252
Jhones Jhonatas <jhones.jhonatas@outlook.com.br>
2024-05-25 00:16:04 -0300

    feat: create server file

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import 'express-async-errors'
+
+import express, { NextFunction, Request, Response } from 'express'
+
+import { AppError } from './errors/app-error'
+
+const app = express()
+const port = process.env.PORT || 80
+
+app.use(express.json())
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      })
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`,
+    })
+  },
+)
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}. 🚀🚀`)
+})
