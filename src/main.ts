bd7c5b322568686e7fe2b2d7b847ba189a34931a
Jhones Jhonatas <jhones.jhonatas@outlook.com.br>
2024-07-18 21:30:14 -0300

    feat: remove console and create auth test structure

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 
-console.log('hello')
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(
---
+++ src/modules/auth/tests/unit/services/sign-in.service.spec.ts
@@ -0,0 +1,1 @@
+describe('sign-in-service', () => {})
